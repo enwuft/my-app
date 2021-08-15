fefd394ac9fbb5d220bbdde7055175577601146a
Vit <enwuft@gmail.com>
2021-08-15 14:29:37 +0700

    chore: fix

--- src/components/widgets/navbar/index.tsx
+++ src/components/widgets/navbar/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Pane } from 'evergreen-ui'
+import Image from 'next/image'
 
 export const Navbar = () => {
   return (
     <>
       <Pane display="flex" padding={2} width="100%" backgroundColor="#101840">
         <Pane className="container mx-auto">
-          <img
+          <Image
             src="/vecskills_bw_logo.png"
             className="bg-cover w-14 p-1"
             alt="Logo"
