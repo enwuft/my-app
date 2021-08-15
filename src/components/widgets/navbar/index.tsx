d1760edeb61cd9d61212d516a0fb1708e3406ff9
Vit <enwuft@gmail.com>
2021-08-15 23:44:57 +0700

    feat: tab navigation view

--- src/components/widgets/navbar/index.tsx
+++ src/components/widgets/navbar/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
-import { Pane } from 'evergreen-ui'
+import {
+  Pane,
+  Heading,
+  Tablist,
+  Tab,
+  Paragraph,
+  Text,
+  majorScale
+} from 'evergreen-ui'
+import { Box, Flex } from 'rebass'
 import Image from 'next/image'
 
 export const Navbar = () => {
@@ -16,6 +25,84 @@ export const Navbar = () => {
           />
         </Pane>
       </Pane>
+
+      <Box>
+        <NavList />
+      </Box>
     </>
+  )
+}
+
+function NavList() {
+  const [selectedIndex, setSelectedIndex] = React.useState(0)
+  const [tabs] = React.useState([
+    'ระดับจังหวัด',
+    'ระดับจังภาค',
+    'ระดับจังประเทศ'
+  ])
+  return (
+    <Box>
+      <Flex justifyContent="center" fontFamily="Boon">
+        <Pane display="flex" padding={4}>
+          <Tablist flexBasis={440}>
+            {tabs.map((tab, index) => (
+              <Tab
+                key={tab}
+                id={tab}
+                onSelect={() => setSelectedIndex(index)}
+                isSelected={index === selectedIndex}
+                aria-controls={`panel-${tab}`}
+              >
+                <Heading size={400}>{tab}</Heading>
+              </Tab>
+            ))}
+          </Tablist>
+        </Pane>
+      </Flex>
+      <Pane>
+        <Flex className="flex bg-gray-50" justifyContent="center">
+          <Box width={[2 / 3]}>
+            <Header />
+            <Pane>
+              {tabs.map((tab, index) => (
+                <Pane
+                  key={tab}
+                  id={`panel-${tab}`}
+                  role="tabpanel"
+                  aria-labelledby={tab}
+                  aria-hidden={index !== selectedIndex}
+                  display={index === selectedIndex ? 'block' : 'none'}
+                >
+                  <Paragraph>Panel {tab}</Paragraph>
+                </Pane>
+              ))}
+            </Pane>
+          </Box>
+        </Flex>
+      </Pane>
+    </Box>
+  )
+}
+
+const Header: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Pane margin={20} display="flex">
+        <Image
+          src="https://res.cloudinary.com/enwuft/image/upload/v1628959441/cad_wv2imb.png"
+          width="100"
+          height="100"
+          alt="logo"
+        />
+        <Box display="flex">
+          <Pane marginTop={10} flexBasis={550}>
+            <Heading size={800}>เขียนเเบบไฟฟ้าด้วยคอมพิวเตอร์</Heading>
+            <Heading size={400} marginTop={5}>
+              ประกาศณียบัตรวิชาชีพชั้นสูง
+            </Heading>
+          </Pane>
+        </Box>
+      </Pane>
+    </React.Fragment>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Pane } from 'evergreen-ui'
+import { Pane, Text } from 'evergreen-ui'
 import Layout from '../components/Layout'
-import NavList from '~/components/widgets/navbar/List'
 
 function Index() {
   return (
     <>
       <Layout title="Home">
         <Pane backgroundColor="#e8e8e8">
-          <NavList />
+          <Text>tab preview</Text>
         </Pane>
       </Layout>
     </>
