46ccd3714182d6fccc54f828aab78df88fb135c6
Vit <enwuft@gmail.com>
2021-08-17 02:19:23 +0700

    add: error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,18 +1,33 @@
-import { Button, Pane } from 'evergreen-ui'
+import * as React from 'react'
+import Image from 'next/image'
+import { NextPage } from 'next'
 
-export default function Error({ statusCode }) {
-  const refreshPage = () => window.location.reload()
+type Props = {
+  statusCode?: number
+}
+
+const Error: NextPage<Props> = ({ statusCode }) => {
   return (
-    <Pane>
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-      <Button onClick={refreshPage} marginRight={16} appearance="minimal">
-        Refresh
-      </Button>
-    </Pane>
+    <div className="flex items-center justify-center space-x-10 mb-60">
+      <div className="flex items-center">
+        <Image
+          src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1610722602/next.egghead.io/pages/broken-eggo.png"
+          width={691}
+          height={493}
+          alt="Error icon"
+        />
+        <div className="flex flex-col max-w-md">
+          <h1 className="font-extrabold text-9xl">
+            {statusCode ? statusCode : 'Error'}
+          </h1>
+          <p>
+            An error has occurred and it is entirely our fault. Can you please
+            click the feedback above and give us any details about what you were
+            trying to view. Sorry for the hassle :/
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
@@ -20,3 +35,5 @@ Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
   return { statusCode }
 }
+
+export default Error
