f018dc853953fdb5e8a4a31dd5d3dc322f583f6f
Vit <enwuft@gmail.com>
2021-08-17 01:06:23 +0700

    chore: _error page

--- readme.md
+++ readme.md
@@ -1,2 +1,25 @@
 ### POC
+
 https://web.dev/code-splitting-with-dynamic-imports-in-nextjs/
+
+`js
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithCustomLoading = dynamic(
+() => import('../components/hello'),
+{ loading: () => <p>...</p> }
+)
+
+function Home() {
+return (
+
+<div>
+<Header />
+<DynamicComponentWithCustomLoading />
+<p>HOME PAGE is here!</p>
+</div>
+)
+}
+
+export default Hom
+`
--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,12 +1,22 @@
 import { Button, Pane } from 'evergreen-ui'
 
-export default function FallbackErr() {
+export default function Error({ statusCode }) {
   const refreshPage = () => window.location.reload()
   return (
     <Pane>
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
       <Button onClick={refreshPage} marginRight={16} appearance="minimal">
         Refresh
       </Button>
     </Pane>
   )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
 }
